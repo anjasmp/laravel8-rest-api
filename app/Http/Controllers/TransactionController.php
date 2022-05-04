c568c3d448cb748baeef0bac5cfa7c3d573ac31d
Anjas Mulya Putra <anjasmulyaputra@gmail.com>
2022-05-04 09:32:24 +0700

    finish

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -56,9 +56,10 @@ class TransactionController extends Controller
             return response()->json($response, Response::HTTP_CREATED);
         } catch (QueryException $e) {
             return response()->json([
-                'message' => "Failed " . $e->errorInfo
+                'message' => "Failed" . $e->errorInfo
             ]);
         }
+
     }
 
     /**
@@ -69,7 +70,13 @@ class TransactionController extends Controller
      */
     public function show($id)
     {
-        //
+        $transaction = Transaction::findOrFail($id);
+        $response = [
+                'message' => 'Detail of transaction resource',
+                'data' => $transaction
+            ];
+
+          return response()->json($response, Response::HTTP_OK);
     }
 
 
@@ -82,7 +89,31 @@ class TransactionController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $transaction = Transaction::findOrFail($id);
+
+        $validator = Validator::make($request->all(), [
+            'title' => ['required'],
+            'amount' => ['required', 'numeric'],
+            'type' => ['required', 'in:expense,revenue']
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        try {
+            $transaction->update($request->all());
+            $response = [
+                'message' => 'Transaction updated',
+                'data' => $transaction
+            ];
+
+            return response()->json($response, Response::HTTP_OK);
+        } catch (QueryException $e) {
+            return response()->json([
+                'message' => "Failed" . $e->errorInfo
+            ]);
+        }
     }
 
     /**
@@ -93,6 +124,20 @@ class TransactionController extends Controller
      */
     public function destroy($id)
     {
-        //
+       $transaction = Transaction::findOrFail($id);
+
+    
+        try {
+            $transaction->delete();
+            $response = [
+                'message' => 'Transaction deleted',
+            ];
+
+            return response()->json($response, Response::HTTP_OK);
+        } catch (QueryException $e) {
+            return response()->json([
+                'message' => "Failed" . $e->errorInfo
+            ]);
+        }
     }
 }
